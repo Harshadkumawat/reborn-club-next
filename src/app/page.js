3d993447fc0a1532658a4a1df7f7cde2e06a68a0
Harshadkumawat <harshadkumawat339977@gmail.com>
2026-02-26 20:30:25 +0530

    Fixed Home page caching

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import GameCard from "@/components/GameCard";
 import { ArrowRight, Trophy, Users, MapPin, Play } from "lucide-react";
 import Link from "next/link";
